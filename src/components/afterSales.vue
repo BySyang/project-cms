264bf545052f195652a60fbff1bed04c3cceeb48
Redbreans <1064593216@qq.com>
2018-04-24 16:48:16 +0800

    sale add

--- src/components/afterSales.vue
+++ src/components/afterSales.vue
@@ -1,15 +1,28 @@
 <template>
-  
+  <div id="afterSales">
+    <div id="search">
+      777797979
+    </div>
+    <div id="form"></div>
+  </div>
 </template>
 <script>
-  export default {
-    data(){
-       return {
-        
-      }
-    }
+export default {
+  data() {
+    return {};
   }
+};
 </script>
-<style lang="scss" scoped>
-  
+<style lang="less" scoped>
+ #afterSales{
+   margin: 1% auto;
+   width: 98%;
+   height: 95%;
+   border: 1px solid red;
+   #search{
+    border: 1px solid #DDDDDD;
+    height: 60px;
+    width: 98%;
+  }
+ } 
 </style>
