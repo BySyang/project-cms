4737ea72e9ae14275e5f1c304e5996ecc18fd95c
LilyLee <1436121892@qq.com>
2018-04-25 12:54:32 +0800

    支付管理

--- src/components/pays.vue
+++ src/components/pays.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="pays_section">
     <div class="pays_content">
-      <div class="top_btnBox">
-        <el-button type="primary">主要按钮</el-button>
+      <div class="topBox">
+        <div>支付管理</div>
+      </div>
+      <div class="pays_centerBox">
+        <div class="centerTop">
+          <span>在线支付方式</span> 
+          <div class="centerContent">
+            <div>
+              
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -12,7 +23,7 @@
 export default {
   data() {
     return {
-      tableData: [{}]
+
     };
   }
 };
@@ -22,23 +33,43 @@ export default {
   overflow: hidden;
   width: 100%;
   height: 100%;
-  background-color: #fefefe;
+  background-color: #f0f0f0;
   .pays_content {
     margin: 1% auto;
     width: 98%;
-    height: 95%;
+    height: 96%;
     background-color: white;
     box-shadow: 0 -3px 0 0 #59ace2;
-    outline: 1px solid red;
-    .top_btnBox {
+    // outline: 1px solid red;
+    .topBox {
+      position: relative;
       height: 30px;
-      padding-left: 1%;
+      padding-left: 22px;
       margin: 1% auto;
       background-color: white;
-      border: 1px solid #f0f0f0;
-      .el-button {
-        padding: 10px 10px;
-        margin: auto 0；;
+      border-bottom: 1px solid #e5e6e6;
+      div {
+        color: white;
+        position: absolute;
+        top: 0;
+        width: 100px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background-color: #59ace2;
+      }
+    }
+    .pays_centerBox{
+      margin-top:1%;
+      height: 92%;
+      // border:1px solid #e5e6e6;
+      .centerTop{
+        height: 30px;
+        padding-left:22px;
+        background:linear-gradient(#fefefe,#f1f1f1) ;
+        span{
+          line-height: 30px;
+        }
       }
     }
   }
