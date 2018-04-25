310547e3265f2a1b0bfed78e41018dc6246da08f
gys13513 <992814002@qq.com>
2018-04-25 18:01:32 +0800

    评价管理

--- src/components/comments.vue
+++ src/components/comments.vue
@@ -7,85 +7,42 @@
       <div class="search">
         <div>
           评论时间:
-          <el-date-picker
-            v-model="xiadandata"
-            type="daterange"
-            align="right"
-            unlink-panels
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            :picker-options="pickerOptions2">
+          <el-date-picker v-model="xiadandata" type="daterange" align="right" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions2">
           </el-date-picker>
         </div>
-        
+
         <div>
           评论人:
-          <el-input
-            placeholder="请输入姓名"
-            prefix-icon="el-icon-search"
-            v-model="goodsName">
+          <el-input placeholder="请输入姓名" prefix-icon="el-icon-search" v-model="goodsName">
           </el-input>
         </div>
 
         <div>
           <el-button type="primary" icon="el-icon-search">搜索</el-button>
         </div>
-        
+
       </div>
       <div class="table">
-        <el-table
-        border
-        ref="multipleTable"
-        :data="data"
-        tooltip-effect="dark"
-        style="width: 100%"
-        >
-          <el-table-column
-            prop="userId"
-            header-align="center"
-            align="center"
-            label="用户名"
-            width="100">
+        <el-table border ref="multipleTable" :data="data1" tooltip-effect="dark" style="width: 100%">
+          <el-table-column prop="userId" header-align="center" align="center" label="用户名" width="100">
           </el-table-column>
-          <el-table-column
-            prop="scoreText"
-            align="center"
-            header-align="center"
-            label="留言内容"
-            show-overflow-tooltip>
+          <el-table-column prop="scoreText" align="center" header-align="center" label="留言内容" show-overflow-tooltip>
           </el-table-column>
-          <el-table-column
-            prop="createTime"
-            align="center"
-            header-align="center"
-            label="留言时间"
-            show-overflow-tooltip>
+          <el-table-column align="center" header-align="center" label="留言时间" show-overflow-tooltip>
+            <template slot-scope="scope">
+              <span style="margin-left: 10px">{{ scope.row.createTime.substr(0,10) }}</span>
+            </template>
           </el-table-column>
-          <el-table-column
-            prop="orderScore"
-            align="center"
-            header-align="center"
-            label="评分"
-            show-overflow-tooltip>
+          <el-table-column prop="orderScore" align="center" header-align="center" label="评分" show-overflow-tooltip>
           </el-table-column>
-          <el-table-column
-            prop="isShow"
-            align="center"
-            header-align="center"
-            label="是否显示"
-            show-overflow-tooltip>
+          <el-table-column prop="isShow" align="center" header-align="center" label="是否显示" show-overflow-tooltip>
           </el-table-column>
         </el-table>
 
         <div class="block">
-  <el-pagination
-    layout="prev, pager, next"
-    :total="1000">
-  </el-pagination>
-</div>
-
-
+          <el-pagination ref="pages" layout="prev, pager, next" :total="total" :page-size="size" @current-change="setCurrent">
+          </el-pagination>
+        </div>
 
       </div>
     </div>
@@ -102,6 +59,8 @@ export default {
       goodsName: "",
       ordersId: "",
       xiadandata: "",
+      current: 1,
+      size: 5,
       //时间选择插件
       pickerOptions2: {
         shortcuts: [
@@ -134,20 +93,34 @@ export default {
           }
         ]
       },
-      data:[]
-    
+      data: []
     };
   },
-  created(){
-    this.getData(); 
-    
+  computed: {
+    data1() {
+      var arr = [];
+      var current = this.current;
+      var size = this.size;
+      for (var i = (current - 1) * size; i < (current - 1) * size + size; i++) {
+        if (this.data[i]) arr.push(this.data[i]);
+      }
+      return arr;
+    },
+    total() {
+      return this.data.length;
+    }
+  },
+  created() {
+    this.getData();
   },
   methods: {
-    getData(){
-      this.$http.get("/goodScoreList").then((res)=>{
-          this.data = res.data.data;
-          console.log(this.data)
-      })
+    getData() {
+      this.$http.get("/goodScoreList").then(res => {
+        this.data = res.data.data;
+      });
+    },
+    setCurrent(val) {
+      this.current = val;
     }
   }
 };
@@ -198,7 +171,7 @@ export default {
         width: 20%;
       }
     }
-    .table{
+    .table {
       width: 96%;
       margin: 10px auto;
     }
