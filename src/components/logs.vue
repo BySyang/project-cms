2210e8485821a0c4a1b19c7714d71560303d9b53
Mr.Wei <1453925044@qq.com>
2018-04-25 09:07:26 +0800

    log改成logs

---
+++ src/components/logs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        系统日志
+    </div>
+</template>
+
+<script>
+   export default {
+    data(){
+       return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
