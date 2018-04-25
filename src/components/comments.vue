1fb650154add551431737e23a8d7146c796b726d
gys13513 <992814002@qq.com>
2018-04-25 16:19:33 +0800

    cc

--- src/components/comments.vue
+++ src/components/comments.vue
@@ -1,15 +1,207 @@
 <template>
-  
-</template>
-<script>
-  export default {
-    data(){
-       return {
+  <div class="orders">
+    <div class="orders_main">
+      <div class="main_top">
+        <div>评论管理</div>
+      </div>
+      <div class="search">
+        <div>
+          评论时间:
+          <el-date-picker
+            v-model="xiadandata"
+            type="daterange"
+            align="right"
+            unlink-panels
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :picker-options="pickerOptions2">
+          </el-date-picker>
+        </div>
         
-      }
+        <div>
+          评论人:
+          <el-input
+            placeholder="请输入姓名"
+            prefix-icon="el-icon-search"
+            v-model="goodsName">
+          </el-input>
+        </div>
+
+        <div>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        </div>
+        
+      </div>
+      <div class="table">
+        <el-table
+        border
+        ref="multipleTable"
+        :data="data"
+        tooltip-effect="dark"
+        style="width: 100%"
+        >
+          <el-table-column
+            prop="userId"
+            header-align="center"
+            align="center"
+            label="用户名"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="scoreText"
+            align="center"
+            header-align="center"
+            label="留言内容"
+            show-overflow-tooltip>
+          </el-table-column>
+          <el-table-column
+            prop="createTime"
+            align="center"
+            header-align="center"
+            label="留言时间"
+            show-overflow-tooltip>
+          </el-table-column>
+          <el-table-column
+            prop="orderScore"
+            align="center"
+            header-align="center"
+            label="评分"
+            show-overflow-tooltip>
+          </el-table-column>
+          <el-table-column
+            prop="isShow"
+            align="center"
+            header-align="center"
+            label="是否显示"
+            show-overflow-tooltip>
+          </el-table-column>
+        </el-table>
+
+        <div class="block">
+  <el-pagination
+    layout="prev, pager, next"
+    :total="1000">
+  </el-pagination>
+</div>
+
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      multipleSelection: [],
+      jiaoyistats: "",
+      goodsName: "",
+      ordersId: "",
+      xiadandata: "",
+      //时间选择插件
+      pickerOptions2: {
+        shortcuts: [
+          {
+            text: "最近一周",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近一个月",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近三个月",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit("pick", [start, end]);
+            }
+          }
+        ]
+      },
+      data:[]
+    
+    };
+  },
+  created(){
+    this.getData(); 
+    
+  },
+  methods: {
+    getData(){
+      this.$http.get("/goodScoreList").then((res)=>{
+          this.data = res.data.data;
+          console.log(this.data)
+      })
     }
   }
+};
 </script>
+
+
 <style lang="scss" scoped>
-  
+.orders {
+  width: 100%;
+  height: 100%;
+  .orders_main {
+    margin: 1% auto;
+    width: 98%;
+    height: 95%;
+    background-color: white;
+    box-shadow: 0 -3px 0 0 #59ace2;
+    .main_top {
+      position: relative;
+      height: 30px;
+
+      border-bottom: 1px solid #e5e6e6;
+      div {
+        position: absolute;
+        top: 0;
+        left: 22px;
+        width: 100px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background-color: #59ace2;
+      }
+    }
+    .search {
+      padding: 10px 0 0 10px;
+      margin-left: 12px;
+      > div {
+        width: 28%;
+        display: inline-block;
+        .el-range-editor.el-input__inner {
+          width: 72%;
+        }
+        .el-input {
+          width: 70%;
+        }
+      }
+      div:nth-of-type(3),
+      div:nth-of-type(4) {
+        width: 20%;
+      }
+    }
+    .table{
+      width: 96%;
+      margin: 10px auto;
+    }
+  }
+}
 </style>
