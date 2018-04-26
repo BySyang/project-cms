fdd3190f9a1d6441f49c616f83839e61588b61b9
summerKun <1344846174@qq.com>
2018-04-26 16:53:28 +0800

    up

--- src/components/orders.vue
+++ src/components/orders.vue
@@ -7,7 +7,7 @@
       <div class="search">
         <div>
           下单时间:
-          <el-date-picker v-model="xiadandata" type="daterange" align="left" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions2">
+          <el-date-picker v-model="xiadandata" type="daterange" align="left" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
           </el-date-picker>
         </div>
         <div>
@@ -40,11 +40,11 @@
           </el-table-column>
           <el-table-column prop="adminRemarks" align="center" header-align="center" label="修改备注" show-overflow-tooltip>
           </el-table-column>
-          <el-table-column prop="newstatus" align="center" header-align="center" label="订单状态" :formatter="formatter" show-overflow-tooltip>
+          <el-table-column prop="newstatus" align="center" header-align="center" label="订单状态"  show-overflow-tooltip>
           </el-table-column>
           <el-table-column prop="orderScore" align="center" header-align="center" label="获得积分" show-overflow-tooltip>
           </el-table-column>
-          <el-table-column prop="newTime" label="下单日期" align="center" header-align="center" width="120" show-overflow-tooltip>
+          <el-table-column prop="newTime" label="下单日期" align="center" header-align="center" width="120" show-overflow-tooltip >
           </el-table-column>
           <el-table-column label="操作" header-align="center" align="center">
             <template slot-scope="scope">
@@ -106,38 +106,6 @@ export default {
         orderScore:'',
         newTime:''
       },
-      //时间选择插件
-      pickerOptions2: {
-        shortcuts: [
-          {
-            text: "最近一周",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit("pick", [start, end]);
-            }
-          },
-          {
-            text: "最近一个月",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              picker.$emit("pick", [start, end]);
-            }
-          },
-          {
-            text: "最近三个月",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-              picker.$emit("pick", [start, end]);
-            }
-          }
-        ]
-      },
       //交易状态
       jiaoyilist: [
         {
@@ -167,7 +135,6 @@ export default {
       userNameList: []
     };
   },
-
   created() {
     this.orsersTableList();
   },
--- src/components/wuliu.vue
+++ src/components/wuliu.vue
@@ -7,7 +7,7 @@
       <div class="search">
         <div>
           下单时间:
-          <el-date-picker v-model="xiadandata" type="daterange" align="left" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions2">
+          <el-date-picker v-model="xiadandata" type="daterange" align="left" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" >
           </el-date-picker>
         </div>
         <div>
@@ -21,14 +21,62 @@
           </el-input>
         </div>
       </div>
-      <div class=""></div>
+      <div class="table">
+        <el-table
+    :data="tableData5"
+    style="width: 100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <el-form label-position="left" inline class="demo-table-expand">
+          <el-form-item label="商品名称">
+            <span>{{ props.row.name }}</span>
+          </el-form-item>
+          <el-form-item label="所属店铺">
+            <span>{{ props.row.shop }}</span>
+          </el-form-item>
+          <el-form-item label="商品 ID">
+            <span>{{ props.row.id }}</span>
+          </el-form-item>
+          <el-form-item label="店铺 ID">
+            <span>{{ props.row.shopId }}</span>
+          </el-form-item>
+          <el-form-item label="商品分类">
+            <span>{{ props.row.category }}</span>
+          </el-form-item>
+          <el-form-item label="店铺地址">
+            <span>{{ props.row.address }}</span>
+          </el-form-item>
+          <el-form-item label="商品描述">
+            <span>{{ props.row.desc }}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="商品 ID"
+      prop="id">
+    </el-table-column>
+    <el-table-column
+      label="商品名称"
+      prop="name">
+    </el-table-column>
+    <el-table-column
+      label="描述"
+      prop="desc">
+    </el-table-column>
+  </el-table>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      xiadandata:'',
+      userName:'',
+      ordersId:'',
+    };
   }
 };
 </script>
@@ -74,6 +122,10 @@ export default {
       div:nth-of-type(3) {
         width: 20%;
       }
+    }
+    .table {
+      width: 96%;
+      margin: 10px auto;
     }
   }
 }
