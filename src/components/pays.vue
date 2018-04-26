c849a630cd2f3de57500c8606e31c0adbd4706a0
LilyLee <1436121892@qq.com>
2018-04-26 16:37:15 +0800

    aaaaaa

--- src/components/pays.vue
+++ src/components/pays.vue
@@ -15,18 +15,18 @@
               <div>支付宝</div>
               <div><img src="../assets/zhifb.jpg" alt=""></div>
               <div>支付宝（中国）网络技术有限公司是国内领先的第三方支付平台，致力于提供“简单、安全、快速”的支付解决方案</div>
-              <div>
-                <el-button type="primary">已启用</el-button>
-                <el-button type="danger">禁用</el-button>
+              <div class="countState">
+                <el-button type="primary" @click="open1">{{message}}</el-button>
+                <el-button type="danger" @click="open2">{{message1}}</el-button>
               </div>
             </div>
             <div class="payment">
               <div>微信支付</div>
               <div><img src="../assets/weixin.jpg" alt=""></div>
               <div>微信支付是集成在微信客户端的支付功能，用户可以通过手机完成快速的支付流程。</div>
-              <div>
-                <el-button type="primary">已启用</el-button>
-                <el-button type="danger">禁用</el-button>
+              <div> 
+                <el-button type="primary" @click="open1();Count()" >{{message}}</el-button>
+                <el-button type="danger" @click="open2();Count2()">{{message1}}</el-button>
               </div>
             </div>
             <div class="payment">
@@ -34,23 +34,84 @@
               <div><img src="../assets/yinlian.jpg" alt=""></div>
               <div>中国银联是通过银联跨行交易清算系统实现商业银行系统间的互联互通和资源共享，保证银行卡跨行、跨地区和跨境使用。</div>
               <div>
-                <el-button type="primary">已启用</el-button>
-                <el-button type="danger">禁用</el-button>
+                <el-button type="primary" @click="open1">{{message}}</el-button>
+                <el-button type="danger" @click="open2">{{message1}}</el-button>
               </div>
             </div>
-              
           </div>
-
         </div>
       </div>
     </div>
   </div>
-
 </template>
 <script>
 export default {
-  data() {
-    return {};
+  data:function() {
+    return {
+      message:"启用",
+      totalCount:0,
+      message1:"禁用",
+      totalCnt:1
+    };
+  },
+  methods: {
+    'Count':function(){
+      var count=this.totalCount;
+      if(count<1){
+        count++;
+        return;
+        console.log(count)                
+      }else if(count>=1){
+        count=1;
+      } 
+    },
+    'Count2':function(){
+      var count1=this.totalCnt;
+      if(count1>0){
+        count1--;
+        console.log(count1)        
+      }else if(count1<1){
+        count1=0;
+      } 
+    },
+    open1() {
+      this.$confirm("是否启用该项？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+      .then(() => {
+        this.$message({
+          type: "success",
+          message: "启用成功!"
+        });
+      })
+      .catch(() => {
+        this.$message({
+          type: "info",
+          message: "已取消启用"
+        });
+      });  
+    },
+    open2() {
+      this.$confirm("禁用后将不能进行与此相关操作，是否禁用该项？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+      .then(() => {
+        this.$message({
+          type: "success",
+          message: "禁用成功!"
+        });
+      })
+      .catch(() => {
+        this.$message({
+          type: "info",
+          message: "已取消禁用"
+        });
+      });  
+    }
   }
 };
 </script>
@@ -96,7 +157,6 @@ export default {
         span {
           line-height: 30px;
         }
-
       }
       .centerContent {
         padding: 10px 22px 0;
